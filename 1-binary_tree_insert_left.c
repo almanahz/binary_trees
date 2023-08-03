0268a4c1de143ef8f4e26d1e9ade4eb1bcccec4f
Saysherief <suhairdama@gmail.com>
2023-08-03 10:55:39 -0700

    Solved Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -26,6 +26,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	else
 	{
 		add->left = parent->left;
+		add->left->parent = add;
 		parent->left = add;
 	}
 	return (add);
