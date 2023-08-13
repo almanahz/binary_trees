63044a6125f36a5460d11e2a21a4bea53c491106
Saysherief <suhairdama@gmail.com>
2023-08-13 19:05:55 +0000

    Solve Depth!

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -2,9 +2,16 @@
 /**
  * binary_tree_depth - depth of specified node from root
  * @tree: node to check the depth
- * Return: 0 is it is the root or number of depth
+ * Return: 0 if it is the root/tree is NULL or number of depth
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+	size_t depth = 0;
+
+	if (tree == NULL || tree->parent == NULL)
+		return (0);
+	else
+		return (++depth);
+	depth += binary_tree_depth(tree->left) + binary_tree_depth(tree->right);
+	return (depth);
 }
